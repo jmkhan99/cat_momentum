5f2fdb1b8cbcf16fa731da52833017ae7f361f5b
jmkhan99 <jmkhana@gmail.com>
2021-08-08 00:29:57 +0900

    20210808 background fix

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@ const body = document.body
 export const bgObj = {
     bgRandomNum : Math.floor(Math.random() * 10) + 1,
     changeBg() {
-        body.style.backgroundImage = `url(../img/cat${this.bgRandomNum}.jpg)`
+        body.style.backgroundImage = `url(./img/cat${this.bgRandomNum}.jpg)`
         console.log(body);
     },
 }
