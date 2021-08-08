f407ba97b7f11e1e2e3ad0504d592fc575d4cbd3
jmkhan99 <jmkhana@gmail.com>
2021-08-08 23:22:47 +0900

    20210808 retest 4

--- js/weather.js
+++ js/weather.js
@@ -7,13 +7,13 @@ export const weatherObj = {
   getWeather(position) {
     this.curLat = position.coords.latitude;
     this.curLon = position.coords.longitude;
-    this.url = `api.openweathermap.org/data/2.5/weather?lat=${this.curLat}&lon=${this.curLon}&appid=59cd4115a815a56a919458303f2f2979`;
-      fetch(this.url)
-        .then((response) => response.json())
-        .then((data) => {
-          console.dir(data);
-          data.nameForm, data.weather[0].main;
-        });
+    this.url = `https://api.openweathermap.org/data/2.5/weather?lat=${this.curLat}&lon=${this.curLon}&appid=59cd4115a815a56a919458303f2f2979`;
+    fetch(this.url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.dir(data);
+        data.nameForm, data.weather[0].main;
+      });
   },
   errWeather() {
     console.log("error");
